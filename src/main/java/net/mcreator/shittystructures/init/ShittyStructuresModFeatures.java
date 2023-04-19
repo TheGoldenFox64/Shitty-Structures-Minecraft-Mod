814de4d924d395948650f431ee179c78a27fb909
TheGoldenFox64 <kungf@10.152.64.87>
2023-04-19 08:28:13 -0700

    made the loot table work, turns out you cant add items that arent obtainiable, like bubble columns and air, (shocking!)     <--- for future reference added FuckChest structure

--- src/main/java/net/mcreator/shittystructures/init/ShittyStructuresModFeatures.java
+++ src/main/java/net/mcreator/shittystructures/init/ShittyStructuresModFeatures.java
@@ -12,10 +12,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraft.world.level.levelgen.feature.Feature;
 
 import net.mcreator.shittystructures.world.features.ores.LandmineFeature;
+import net.mcreator.shittystructures.world.features.FuckchestFeature;
 import net.mcreator.shittystructures.ShittyStructuresMod;
 
 @Mod.EventBusSubscriber
 public class ShittyStructuresModFeatures {
 	public static final DeferredRegister<Feature<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.FEATURES, ShittyStructuresMod.MODID);
 	public static final RegistryObject<Feature<?>> LANDMINE = REGISTRY.register("landmine", LandmineFeature::feature);
+	public static final RegistryObject<Feature<?>> FUCKCHEST = REGISTRY.register("fuckchest", FuckchestFeature::feature);
 }
